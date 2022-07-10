eff564a936b4b5b63667497502ab1a3820ae8986
zuhaib-ali <zuhaibsoomro25@gmail.com>
2022-07-10 19:39:28 +0500

    7:39 change made at

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import TextForm from './Components/TextForm'
 
 function App() {
   return (
-    <>
+    <React.Fragment>
       <Navbar/>
       <TextForm />
-    </>
+    </React.Fragment>
   );
 }
 
